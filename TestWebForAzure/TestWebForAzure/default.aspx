e709fff6e9b604ad1eb5c58609c17275c82c0019
kuro <kuro_q@outlook.com>
2016-06-19 22:08:17 +0800

    add default asp.net page

---
+++ TestWebForAzure/TestWebForAzure/default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="TestWebForAzure._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Hello, This is a default page!</h1>
+            <p>Test for Azure</p>
+            <asp:Label ID="lblMessage" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ TestWebForAzure/TestWebForAzure/default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TestWebForAzure
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var key = "mysettings";
+                var value = ConfigurationManager.AppSettings[key];
+                lblMessage.Text = value ?? "Empty";
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = ex.Message;
+            }
+        }
+    }
+}
